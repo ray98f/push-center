57dcca9ea7f6660742269b45925c06b3cd7bbe85
chentong <978767524@qq.com>
2020-12-14 17:32:37 +0800

    add: 数据库相关配置

---
+++ src/main/java/com/zte/msg/pushcenter/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.zte.msg.pushcenter.utils;
+
+/**
+ * description:
+ *
+ * @author chentong
+ * @version 1.0
+ * @date 2020/12/14 16:52
+ */
+public class Constants {
+
+    public static long DATA_NOT_DELETED = 0;
+}
