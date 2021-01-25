a69deded535d8726e1ff684f06d07d4dc44f16e9
chentong <aChen1874@163.com>
2021-01-25 14:31:48 +0800

    fix: 添加电话号码和邮箱地址校验

--- pc-core/src/main/java/com/zte/msg/pushcenter/pccore/controller/PushCenterController.java
+++ pc-core/src/main/java/com/zte/msg/pushcenter/pccore/controller/PushCenterController.java
@@ -12,18 +12,17 @@ import com.zte.msg.pushcenter.pccore.dto.req.WeChatMessageReqDTO;
 import com.zte.msg.pushcenter.pccore.enums.ErrorCode;
 import com.zte.msg.pushcenter.pccore.exception.CommonException;
 import com.zte.msg.pushcenter.pccore.service.PushCenterService;
-import com.zte.msg.pushcenter.pccore.utils.Constants;
-import com.zte.msg.pushcenter.pccore.utils.SignUtils;
 import com.zte.msg.pushcenter.pccore.utils.PatternUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.Objects;
 
 /**
