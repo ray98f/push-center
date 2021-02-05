28a4653e6e49a3fb7ed0ce227226cb30cd62180c
chentong <aChen1874@163.com>
2021-02-05 17:25:34 +0800

    fix:

--- pc-core/src/main/java/com/zte/msg/pushcenter/pccore/utils/DateUtils.java
+++ pc-core/src/main/java/com/zte/msg/pushcenter/pccore/utils/DateUtils.java
@@ -2,6 +2,7 @@ package com.zte.msg.pushcenter.pccore.utils;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * description:
@@ -12,19 +13,22 @@ import java.util.Date;
  */
 public class DateUtils {
 
+    static {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC+8"));
+    }
 
     public static String formatDate(Date date) {
 
         return org.apache.http.client.utils.DateUtils.formatDate(date, Constants.DATE_FORMAT_PATTERN);
     }
-
     public static Timestamp zeroClockOf(long mill) {
+
 
         return new Timestamp(mill - mill % Constants.DAY_MILL);
     }
 
     public static Timestamp sevenDayBefore(long mill) {
-        return new Timestamp(mill - mill % Constants.DAY_MILL + Constants.DAY_MILL * 7);
+        return new Timestamp(mill - mill % Constants.DAY_MILL - Constants.DAY_MILL * 7);
     }
 
     public static Timestamp now() {
@@ -33,5 +37,10 @@ public class DateUtils {
 
     public static Timestamp now(long mill) {
         return new Timestamp(mill);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(zeroClockOf(System.currentTimeMillis()));
+        System.out.println(sevenDayBefore(System.currentTimeMillis()));
     }
 }
