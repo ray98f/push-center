126be14f57a04ef7b4eb0b8db72dbc51bf50baed
Ray <403027278@qq.com>
2021-02-18 11:19:33 +0800

    历史记录时间搜索bug修复

--- pc-core/src/main/java/com/zte/msg/pushcenter/pccore/dto/req/MailHistoryReqDTO.java
+++ pc-core/src/main/java/com/zte/msg/pushcenter/pccore/dto/req/MailHistoryReqDTO.java
@@ -1,14 +1,10 @@
 package com.zte.msg.pushcenter.pccore.dto.req;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.zte.msg.pushcenter.pccore.dto.PageReqDTO;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.util.Date;
 
 /**
  * @author frp
@@ -28,14 +24,10 @@ public class MailHistoryReqDTO extends PageReqDTO {
     private String receiveAddress;
 
     @ApiModelProperty(value = "搜索开始时间")
-    @DateTimeFormat(pattern = "yyyy-MM-DD HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
-    private Date startTime;
+    private String startTime;
 
     @ApiModelProperty(value = "搜索结束时间")
-    @DateTimeFormat(pattern = "yyyy-MM-DD HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
-    private Date endTime;
+    private String endTime;
 
     @ApiModelProperty(value = "发送状态")
     private Integer result;
--- pc-core/src/main/java/com/zte/msg/pushcenter/pccore/dto/req/StatisticsReqDTO.java
+++ pc-core/src/main/java/com/zte/msg/pushcenter/pccore/dto/req/StatisticsReqDTO.java
@@ -1,14 +1,10 @@
 package com.zte.msg.pushcenter.pccore.dto.req;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.zte.msg.pushcenter.pccore.dto.PageReqDTO;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import java.util.Date;
 
 /**
  * @author frp
@@ -19,12 +15,8 @@ import java.util.Date;
 public class StatisticsReqDTO extends PageReqDTO {
 
     @ApiModelProperty(value = "搜索开始时间")
-    @DateTimeFormat(pattern = "yyyy-MM-DD HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
-    private Date startTime;
+    private String startTime;
 
     @ApiModelProperty(value = "搜索结束时间")
-    @DateTimeFormat(pattern = "yyyy-MM-DD HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-DD HH:mm:ss")
-    private Date endTime;
+    private String endTime;
 }
